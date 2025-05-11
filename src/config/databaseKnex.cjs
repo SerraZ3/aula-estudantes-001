4445b2920be6fca7abdb06fac9e674aa0c7ecf1e
SerraZ3 <serra.henrique3@gmail.com>
2025-05-11 20:31:57 -0300

    feat:adicionando configuração windows

--- src/config/databaseKnex.cjs
+++ src/config/databaseKnex.cjs
@@ -5,7 +5,7 @@ const database = process.env.POSTGRES_DB || 'question';
 const host = process.env.POSTGRES_HOST || '127.0.0.1';
 const username = process.env.POSTGRES_USER;
 const password = process.env.POSTGRES_PASSWORD;
-const port = process.env.POSTGRES_PORT;
+const port = process.env.POSTGRES_PORT || 5432;
 
 module.exports = {
   client: 'pg',
